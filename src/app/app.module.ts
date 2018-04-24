01c1db73f39d19aa8f655cb30446a88800bcb340
deechris27 <deechris27@dee.com>
2018-04-24 19:00:20 +0530

    full app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9awYg5cN8afWcTz3krbDaCvj2r0rWVy0",
+  authDomain: "chatapp-1d86a.firebaseapp.com",
+  databaseURL: "https://chatapp-1d86a.firebaseio.com",
+  projectId: "chatapp-1d86a",
+  storageBucket: "chatapp-1d86a.appspot.com",
+  messagingSenderId: "795342121605"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
